10bf6a61595c47c2e2108cf997733ac272275b4d
김경훈 <gyeong1181@gmail.com>
2025-07-31 22:09:28 +0900

    XRP 튜닝완료, BTC 튜닝은 안 함.

--- systemTrading/EMA_RSI/config.py
+++ systemTrading/EMA_RSI/config.py
@@ -16,11 +16,11 @@ SYMBOLS = ["BTC/USDT", "ETH/USDT", "XRP/USDT", "SOL/USDT", "AVAX/USDT"]
 
 # 각 티커별 상세 파라미터 설정 (튜닝 중인 경우, 튜닝 결과를 여기에 반영합니다.)
 SYMBOL_PARAMS = {
-    "BTC/USDT": {
+    "BTC/USDT": { # 초기에 트레이딩뷰로 튜닝한 결과
         "timeframe": "1h",
         "tp": 0.07,
         "sl": 0.02,
-        "position_size": 0.001,
+        "position_size": 0.0003, # 50달러 정도
         "ema_fast": 5,
         "ema_slow": 20,
         "rsi_period": 14,
@@ -28,7 +28,7 @@ SYMBOL_PARAMS = {
         "leverage": 5, 
     },
     "ETH/USDT": { # 2025.07.30에 튜닝함.
-        "timeframe": "1h",
+        "timeframe": "1h", # 튜닝 테스트 기간 24.01.01~25.07.30 이며, 샤프지수 약 6으로 나옴.
         "tp": 0.03178,
         "sl": 0.02499,
         "position_size": 0.01,
@@ -38,15 +38,15 @@ SYMBOL_PARAMS = {
         "rsi_threshold": 64,
         "leverage": 5, 
     },
-    "XRP/USDT": {
-        "timeframe": "1h",
-        "tp": 0.05,
-        "sl": 0.025,
-        "position_size": 10,
-        "ema_fast": 7,
-        "ema_slow": 25,
+    "XRP/USDT": { # 2025.07.31에 튜닝함.  
+        "timeframe": "1h", # 튜닝 테스트 기간 24.01.01~25.07.30 이며, 샤프지수 약 16으로 아주 높게 나옴.
+        "tp": 0.0488,
+        "sl": 0.0111,
+        "position_size": 10, # 약 20달러 정도
+        "ema_fast": 16,
+        "ema_slow": 66,
         "rsi_period": 14,
-        "rsi_threshold": 60,
+        "rsi_threshold": 65,
         "leverage": 5, 
     },
     "SOL/USDT": {
